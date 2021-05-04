208d4915ce83ab50ea81736031b70ed9be82f979
Chrisite L Richardson <crichardson@catholiccharities.net>
2021-05-04 18:34:05 -0500

    Initial Commit

---
+++ TBS Profile/TBS_ProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  TBS_ProfileApp.swift
+//  TBS Profile
+//
+//  Created by admin on 5/4/21.
+//
+
+import SwiftUI
+
+@main
+struct TBS_ProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
